bc0a6151e7a570a490073f5637e6f5467685a263
mrityunjay9693 <mkaykumar123@gmail.com>
2024-02-25 02:18:13 +0530

    login page

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hostel Web Application</title>
+</head>
+<body>
+    <div>
+        <form action="Authenticate.php" method="POST"> <!--Here, we have to submit data to php -->
+            <input type="text" name="userid" id="userid" placeholder="User Id">
+            <input type="password" name="password" id="password" placeholder="Password">
+            <button>Login</button>
+            <label name="error_message" id="error_message"></label>
+        </form>
+    </div>    
+</body>
+</html>

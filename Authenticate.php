db593d5e865b7c31bc25e6fe07caf5d02566b97f
mrityunjay9693 <mkaykumar123@gmail.com>
2024-03-16 22:37:19 +0530

    Authentication

--- Authenticate.php
+++ Authenticate.php
@@ -17,15 +17,16 @@ $stmt = $con->prepare("SELECT user_id FROM user WHERE user_id = ? AND password =
 $stmt->bind_param("ss", $user_id, $password);
 $stmt->execute();
 $result = $stmt->get_result();
-
+echo "Hello";
 if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-    session_start();
-    $_SESSION['user_id'] = $user_id;
-    header('Location:Dashboard.php');
-    //utility::js_redirect('./Dashboard.php/');
+    //$row = $result->fetch_assoc();
+    // session_start();
+    // $_SESSION['user_id'] = $user_id;
+    //header('Location: Dashboard.php');
+    //echo "Hello in";
+    utility::js_redirect("Dashboard.php");
 } else {
     utility::js_alert("Invalid username or password.");
-    //utility::js_redirect('/index.php');
-    header('Location:index.php');
+    utility::js_redirect('index.php');
+    //header('Location:index.php');
 }

3af29a9050dc184c7e998bad3e36e348289e33a9
mrityunjay9693 <mkaykumar123@gmail.com>
2024-03-20 20:40:58 +0530

    index

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
+<?php
+require "Utility.php";
+session_start();
+if (isset($_SESSION['user_id'])) {
+    utility::js_redirect("Dashboard.php");
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hostel Web Application</title>
 </head>
+
 <body>
     <div>
         <form action="Authenticate.php" method="POST"> <!--Here, we have to submit data to php -->
@@ -13,6 +23,7 @@
             <button>Login</button>
             <label name="error_message" id="error_message"></label>
         </form>
-    </div>    
+    </div>
 </body>
+
 </html>

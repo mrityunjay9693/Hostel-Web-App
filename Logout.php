2dcae18c9e929229aa7647a0a5e3c32ea62a3db5
mrityunjay9693 <mkaykumar123@gmail.com>
2024-04-01 13:40:01 +0530

    log out module

--- Logout.php
+++ Logout.php
@@ -9,6 +9,7 @@ session_destroy();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <title>Logout</title>
 </head>
 

ea896213e28be637f429bc9465c95ea81cf6b31f
mrityunjay9693 <mkaykumar123@gmail.com>
2024-04-01 13:39:44 +0530

    index

--- index.php
+++ index.php
@@ -12,17 +12,20 @@ if (isset($_SESSION['user_id'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hostel Web Application</title>
+    <link rel="stylesheet" href="styles.css">
+    <title>Hostel Management System</title>
 </head>
 
 <body>
     <div>
-        <form action="Authenticate.php" method="POST"> <!--Here, we have to submit data to php -->
-            <input type="text" name="user_id" id="user_id" placeholder="User Id">
-            <input type="password" name="password" id="password" placeholder="Password">
-            <button>Login</button>
-            <label name="error_message" id="error_message"></label>
-        </form>
+        <h1 style="color:green">HMS 1.0</h1>
+        <h1>Login</h2>
+            <form action="Authenticate.php" method="POST" class="log-in"> <!--Here, we have to submit data to php -->
+                <input type="text" name="user_id" id="user_id" placeholder="User Id"><br><br>
+                <input type="password" name="password" id="password" placeholder="Password"><br><br>
+                <button style="color:blue">Login</button>
+                <label name="error_message" id="error_message"></label>
+            </form>
     </div>
 </body>
 

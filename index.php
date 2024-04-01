ae902580b96c8d34b41ab10659bccfa0ee1820f3
mrityunjay9693 <mkaykumar123@gmail.com>
2024-04-01 20:21:14 +0530

    login css done and logo added.

--- index.php
+++ index.php
@@ -12,20 +12,20 @@ if (isset($_SESSION['user_id'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
     <title>Hostel Management System</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 </head>
 
 <body>
-    <div>
-        <h1 style="color:green">HMS 1.0</h1>
-        <h1>Login</h2>
-            <form action="Authenticate.php" method="POST" class="log-in"> <!--Here, we have to submit data to php -->
-                <input type="text" name="user_id" id="user_id" placeholder="User Id"><br><br>
-                <input type="password" name="password" id="password" placeholder="Password"><br><br>
-                <button style="color:blue">Login</button>
-                <label name="error_message" id="error_message"></label>
-            </form>
+    <div class="container">
+        <form action="Authenticate.php" method="POST" class="log-in"> <!--Here, we have to submit data to php -->
+            <img src="images/Logo.jpg" alt="Logo" class="logo-type-1"><br>
+            <input type="text" name="user_id" id="user_id" placeholder="User Id" class="form-control-block">
+            <input type="password" name="password" id="password" placeholder="Password" class="form-control-block">
+            <button class="form-control-block submit">Login</button>
+            <label name="error_message" id="error_message"></label>
+        </form>
     </div>
 </body>
 
